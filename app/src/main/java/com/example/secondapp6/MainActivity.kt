b388801e55e7057599feb45f902038e4637f42f6
ji-circle <sp202401jw@gmail.com>
2024-10-06 20:29:04 +0900

    add: fab

--- app/src/main/java/com/example/secondapp6/MainActivity.kt
+++ app/src/main/java/com/example/secondapp6/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -40,22 +44,27 @@ class MainActivity : ComponentActivity() {
                             title = { Text("Top App Bar") }
                         )
                     },
-                    //바텀바 넣기
                     bottomBar = {
-                        //BottomAppBar 선택할 때 action 어쩌구 없는거로
                         BottomAppBar(
                             containerColor = MaterialTheme.colorScheme.primaryContainer,
                             contentColor = MaterialTheme.colorScheme.primary
                         ) {
-                            //뭔가를 넣을 수 있음.
-                            //텍스트 정렬하려면 textAlign을 괄호 뒤에 추가하기.
-                            //  근데 그냥  textAlign = TextAlign.Center 이라고 하면, 텍스트의 범위가 텍스트에 딱 맞춰있는 상태라서
-                            //    정렬이 안 됨... 텍스트가 차지하고 있는 범위를 bottom app bar의 전체로 해줘야 가운데 정렬이 됨...
-                            //       = modifier 사용하라는 뜻
                             Text(
                                 modifier = Modifier.fillMaxWidth(),
                                 textAlign = TextAlign.Center,
-                                text = "Bottom App Bar")
+                                text = "Bottom App Bar"
+                            )
+                        }
+                    },
+
+                    //scaffold 안에 fab가 하나의 컴포넌트로 되어있음! (클릭 가능 )
+                    //fab도 람다함수로 넣기
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {}) {
+                            //Icon 선택 시 이미지벡터가 있는 거를 선택하기
+                            //시각장애인이나 이거에 대한 설명이 필요한 경우를 위해 넣는 것...
+                            Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+
                         }
 
                     }
